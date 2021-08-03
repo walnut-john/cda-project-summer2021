127120f56792321a9c02171073f967fb668c9fb3
John Walker <johnhbwalker@icloud.com>
2021-08-02 23:36:36 -0400

    adds dependencies and corrects variable names

--- save_load_models.py
+++ save_load_models.py
@@ -1,20 +1,19 @@
 import pickle
-
+import xgboost as xgb
+import tensorflow as tf
 
 # Sci-Kit Learn
 def save_scikit_model(model, filename='mlp_model.sav'):
-    pickle.dump(mlp_model, open(filename, 'wb'))
-
+    pickle.dump(model, open(filename, 'wb'))
 
 def load_scikit_model(filename='mlp_model.sav'):
     loaded_model = pickle.load(open(filename, 'rb'))
-    #result = loaded_model.score(X_test, Y_test)
     return loaded_model
 
 
 # XGBoost
 def save_xgb_model(model, filename='xgb_model.json'):
-    xgb_model.save_model(filename)
+    model.save_model(filename)
 
 def load_xgb_model(filename='xgb_model.json'):
     model2 = xgb.XGBClassifier()
